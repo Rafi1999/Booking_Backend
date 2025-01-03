223dd226db931dedb56fbdc92f8bbd02e4e6f4ef
Al Rafi <alrafi4@gmail.com>
2025-01-03 21:55:15 +0600

    uppp

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const bookingRoutes = require('./routes/bookingRoutes');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -22,7 +21,5 @@ app.get('/', (req, res) => {
   res.send('Restaurant Booking API is running...');
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
